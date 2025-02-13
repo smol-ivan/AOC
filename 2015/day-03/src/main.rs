97847a71e1cdcd04c11dc1895d1b8431ce7d6319
Ivan Gordillo <gordillosolis@icloud.com>
2025-02-13 16:05:15 -0600

    finished day 3 2015

--- 2015/day-03/src/main.rs
+++ 2015/day-03/src/main.rs
@@ -1,10 +1,12 @@
-use std::collections::HashMap;
-use std::fs;
+use std::{collections::HashSet, fs};
 
 fn main() {
-    println!("Hello, world!");
+    let input = read_input_file();
+    part_1(input.clone());
+    part_2(input);
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Point {
     x: i32,
     y: i32,
@@ -35,18 +37,63 @@ impl Point {
     }
 }
 
-fn part_1(input: Vec<char>) {
-    let mut places: HashMap<Point> = HashMap::new();
+fn part_2(input: Vec<char>) {
+    let mut santa = Point::new(0, 0);
+    let mut bot = Point::new(0, 0);
 
+    let mut flag = false;
+
+    let mut visited: HashSet<Point> = HashSet::new();
+
+    visited.insert(santa.clone());
+
+    for direction in input.iter() {
+        let tracker = match flag {
+            false => &mut santa,
+            true => &mut bot,
+        };
+
+        match direction {
+            '>' => tracker.right(),
+            '<' => tracker.left(),
+            '^' => tracker.up(),
+            'v' => tracker.down(),
+            _ => todo!(),
+        };
+
+        visited.insert(tracker.clone());
+        flag = !flag;
+    }
+
+    let result = visited.len();
+
+    println!("Parte 2 -> {}", result);
+}
+
+// Usar hashset en lugar de un vector ya que en el pero de los casos tendre una busqueda O(n)
+fn part_1(input: Vec<char>) {
     let mut tracker = Point::new(0, 0);
 
-    input.iter().map(|c| match c {
-        '>' => tracker.right(),
-        '<' => tracker.left(),
-        '^' => tracker.up(),
-        'v' => tracker.down(),
-        _ => todo!(),
-    });
+    let mut visited: Vec<Point> = Vec::new();
+    visited.push(tracker.clone());
+
+    for direction in input.iter() {
+        match direction {
+            '>' => tracker.right(),
+            '<' => tracker.left(),
+            '^' => tracker.up(),
+            'v' => tracker.down(),
+            _ => todo!(),
+        };
+
+        if !visited.contains(&tracker) {
+            visited.push(Point::new(tracker.x, tracker.y));
+        }
+    }
+
+    let result = visited.len();
+
+    println!("This many houses with at least one present -> {}", result);
 }
 
 fn read_input_file() -> Vec<char> {
@@ -54,5 +101,6 @@ fn read_input_file() -> Vec<char> {
 
     let input: Vec<char> = file.trim().chars().collect();
 
+    println!("input lenght-> {}", &input.len());
     input
 }
